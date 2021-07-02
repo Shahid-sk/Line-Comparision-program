88d731707b9a19cf85da303104b43951a0b54a47
Shahid-sk <mail2sk.shahid@gmail.com>
2021-07-02 06:35:53 +0530

    first commit

---
+++ src/LineComparision/Linecomparision.java
@@ -0,0 +1,7 @@
+package LineComparision;
+
+public class Linecomparision {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Line Comparision Computation program");
+	}
+}
